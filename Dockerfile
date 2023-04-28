79244cdd2a943dd87930cd87955671fae496b182
maelgueguen <mael.gueguen@efrei.net>
2023-04-28 10:51:19 +0200

    ras

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.10
 ENV API_KEY="14c52ccb6470e53bd70d27f06e1b5841"
 WORKDIR /app
+RUN pip install requests
 
 COPY . .
 CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ latitude=os.environ['LAT']
 longitude=os.environ['LONG']
 YOUR_API_KEY=os.environ['API_KEY']
 
-def get_weather(latitude,longitude,YOU_API_KEY):
+def get_weather(latitude,longitude,YOUR_API_KEY):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={YOUR_API_KEY}&units=metric"
     response = requests.get(url)
     print(response.json())
