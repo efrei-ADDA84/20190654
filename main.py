72fdfd4a59287b907aac2d7c776eec444acf293f
maelgueguen <mael.gueguen@efrei.net>
2023-04-28 16:21:58 +0200

    ras

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ app = FastAPI()
 
 YOUR_API_KEY=os.environ['API_KEY']
 
-@app.post("/")
-async def get_weather(lat,lon):
+@app.get("/")
+async def get_weather():
+
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={YOUR_API_KEY}&units=metric"
     response = requests.get(url)
     if response.status_code != 200:
